865afcd5ffeadbca89d66198e1af38996923d43d
Petar <sjuesju@yahoo.no>
2020-07-15 14:05:43 +0300

    added some user input verification

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
-$("#btn").on("click", function(event){
+$("#btn").on("click", function(){
 	let gameObject = {gameboard:$('#gameboard').val(), cell:$('#cell').val(), direction:$('#direction').val()};
 	checkForEmptyInputs(gameObject);
 });
 
-function checkForEmptyInputs(gameObject){
-	const keys = Object.keys(gameObject)
-	for (const key of keys) {
-  		console.log(key)
-  		checkForEmptyInput(gameObject.key, key);
+$("#gameboard").on("input", checkForCorrectlyAddedGameboard);
+
+function checkForCorrectlyAddedGameboard(){
+
+	if (isGameboardInputOk($('#gameboard').val().trim().split('')) == false){
+		console.log('False');
+	}	
+}
+
+function isGameboardInputOk(gameboardInput){
+	let len = gameboardInput.length, squareBraketOn = false;
+	removeEmptyElementsInGameboardInput();
+
+	if (gameboardInput[0] != '[' || gameboardInput[len - 1] != ']'){
+		console.log(gameboardInput[0])
+		return false;
 	}
+	for (var i = 1; i < len - 1; i++) {
+		if (gameboardInput[i].length > 0){			
+			if (gameboardInput[i] == '['){
+				if (squareBraketOn == true){
+					return false;
+				}
+				squareBraketOn = true;
+			} else if (gameboardInput[i] == ']'){
+				if (squareBraketOn == false){
+					return false;
+				}
+				squareBraketOn = false;
+			} else if (gameboardInput[i] == "'"){
+				if((gameboardInput[i - 1] == '[' || gameboardInput[i - 1] == ',') && (gameboardInput[i + 1] == 'R' || gameboardInput[i + 1] == 'G' || gameboardInput[i + 1] == 'B' || gameboardInput[i + 1] == 'O' || gameboardInput[i + 1] == 'P' || gameboardInput[i + 1] == 'Y' || gameboardInput[i + 1] == ',' || gameboardInput[i + 1] == ']')){
+					// it is ok
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+}
 
-
-	// for (const [key, value] of Object.entries(object1)) {
- //  		console.log(`${key}: ${value}`);
-	// }
-	// checkForEmptyInput(gameObject.gameboard, 'gameboard');
-	// checkForEmptyInput(cell, 'cell');
-	// checkForEmptyInput(direction, 'direction');
+function checkForEmptyInputs(gameObject){
+	$('.error').remove();
+	checkForEmptyInput(gameObject.gameboard, 'gameboard');
+	checkForEmptyInput(gameObject.cell, 'cell');
+	checkForEmptyInput(gameObject.direction, 'direction');
 }
 
 function checkForEmptyInput(variableResult, variableName){
