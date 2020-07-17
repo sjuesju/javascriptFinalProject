4c81e751182bf18df0930a82273e6d55c82e830d
Petar <sjuesju@yahoo.no>
2020-07-17 11:07:05 +0300

    Finished js validation on the input

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 $("#btn").on("click", function(){
 	let gameObject = {gameboard:$('#gameboard').val(), cell:$('#cell').val(), direction:$('#direction').val()};
-	checkForEmptyInputs(gameObject);
+	if (checkForEmptyInputs(gameObject) == false){
+		return false;
+	}
+	// php validation
+	if (checkIsMoveWithinArray(gameObject) == false){
+		return false;
+	}
+
 });
 
 $("#gameboard").on("input", checkForCorrectlyAddedGameboard);
+$("#cell").on("input", checkForCorrectlyAddedCell);
+$("#direction").on("input", checkForCorrectlyAddedDirection);
+
+function checkIsMoveWithinArray(gameObject){
+	let arraySize = getArraySize(gameObject.gameboard);
+}
+
+function getArraySize(gameboard){
+	let splittedGameboard = gameboard.split('['), splittedGameboardElement = splittedGameboard[2].split(','), final = [];
+	final.push(splittedGameboard.length - 2);
+	final.push(splittedGameboardElement.length - 1);
+	return final;
+}
+
+function checkForCorrectlyAddedDirection(){
+	let direction = $('#direction'), directionInput = removeEmptyElementsInInputReturnString(direction.val());
+	if (directionInput == 'R' || directionInput == 'L' || directionInput == 'U' || directionInput == 'D'){
+			direction.css('border-color', 'green');
+	} else {
+		direction.css('border-color', 'red');
+	}
+}
 
 function checkForCorrectlyAddedGameboard(){
-	let gameBoard = $('#gameboard'), gameboardInput = removeEmptyElementsInGameboardInput(gameBoard.val());
+	let gameBoard = $('#gameboard'), gameboardInput = removeEmptyElementsInInput(gameBoard.val());
 	if (isGameboardInputOk(gameboardInput) == false){
 		gameBoard.css('border-color', 'red');
 	} else {
@@ -14,11 +43,35 @@ function checkForCorrectlyAddedGameboard(){
 	}
 }
 
-function removeEmptyElementsInGameboardInput(gameboardInput){
-	let temp = gameboardInput.split(''), len = gameboardInput.length, final = [];
+function checkForCorrectlyAddedCell(){
+	let cell = $('#cell'), cellInput = removeEmptyElementsInInputReturnString(cell.val());
+	if (isCellInputOk(cellInput) == false){
+		cell.css('border-color', 'red');
+	} else {
+		cell.css('border-color', 'green');
+	}
+}
+
+function isCellInputOk(cellInput){
+	var regex = /^\[\d{1,}\,\d{1,}\]$/;
+	return regex.test(cellInput);
+}
+
+function removeEmptyElementsInInput(input){
+	let temp = input.split(''), len = input.length, final = [];
 	for (var i = 0; i < len; i++) {
 		if (temp[i].trim().length > 0){
 			final.push(temp[i]);
+		}
+	}
+	return final;
+}
+
+function removeEmptyElementsInInputReturnString(input){
+	let temp = input.split(''), len = input.length, final = "";
+	for (var i = 0; i < len; i++) {
+		if (temp[i].trim().length > 0){
+			final += temp[i];
 		}
 	}
 	return final;
@@ -38,6 +91,8 @@ function isGameboardInputOk(gameboardInput){
 				}
 				squareBraketOn = true;
 				comaIndex++;
+				colorIndex++;
+				colonIndex++;
 			} else if (gameboardInput[i] == ']'){
 				if (squareBraketOn == false){
 					return false;
@@ -45,7 +100,11 @@ function isGameboardInputOk(gameboardInput){
 				squareBraketOn = false;
 			} else if (gameboardInput[i] == "'"){
 				if ((gameboardInput[i - 1] == '[' || gameboardInput[i - 1] == ',' || gameboardInput[i - 1] == 'R' || gameboardInput[i - 1] == 'G' || gameboardInput[i - 1] == 'B' || gameboardInput[i - 1] == 'O' || gameboardInput[i - 1] == 'P' || gameboardInput[i - 1] == 'Y' || gameboardInput[i - 1] == ',') && (gameboardInput[i + 1] == 'R' || gameboardInput[i + 1] == 'G' || gameboardInput[i + 1] == 'B' || gameboardInput[i + 1] == 'O' || gameboardInput[i + 1] == 'P' || gameboardInput[i + 1] == 'Y' || gameboardInput[i + 1] == ',' || gameboardInput[i + 1] == ']')){
-					// it is ok
+					if (typeof colonCounter[colonIndex] == 'undefined'){
+						colonCounter[colonIndex] = 1;
+					} else {					
+						colonCounter[colonIndex]++;
+					}
 				} else {
 					return false;
 				}
@@ -64,13 +123,26 @@ function isGameboardInputOk(gameboardInput){
 				}			
 
 			} else if (gameboardInput[i] == "R" || gameboardInput[i] == "G" || gameboardInput[i] == "B" || gameboardInput[i] == "O" || gameboardInput[i] == "P" || gameboardInput[i] == "Y"){
-				// it is ok
+				if (typeof colorCounter[colorIndex] == 'undefined'){
+						colorCounter[colorIndex] = 1;
+					} else {					
+						colorCounter[colorIndex]++;
+					}
 			} else {
 				return false;
 			}
 		}
 	}
-	return compareNumberOfElementsInEveryRow(comaCounter);
+	if (compareNumberOfElementsInEveryRow(comaCounter) == false){
+		return false;
+	}
+	if (compareNumberOfElementsInEveryRow(colonCounter) == false){
+		return false;
+	}
+	if (compareNumberOfElementsInEveryRow(colorCounter) == false){
+		return false;
+	}
+	return true;
 }
 
 function compareNumberOfElementsInEveryRow(comaCounter){
@@ -88,15 +160,25 @@ function compareNumberOfElementsInEveryRow(comaCounter){
 }
 
 function checkForEmptyInputs(gameObject){
+	let flag = true;
 	$('.error').remove();
-	checkForEmptyInput(gameObject.gameboard, 'gameboard');
-	checkForEmptyInput(gameObject.cell, 'cell');
-	checkForEmptyInput(gameObject.direction, 'direction');
+	if (checkForEmptyInput(gameObject.gameboard, 'gameboard') == false){
+		flag = false;
+	}
+	if (checkForEmptyInput(gameObject.cell, 'cell') == false){
+		flag = false;
+	}
+
+	if (checkForEmptyInput(gameObject.direction, 'direction') == false){
+		flag = false;
+	}
+	return flag;
 }
 
 function checkForEmptyInput(variableResult, variableName){
 	if(variableResult.trim().length == 0){
 		$('#' + variableName).after('<span class="error">Field Required</span>');
+		return false;
 	}
 }
 
